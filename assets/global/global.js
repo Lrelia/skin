48954190056b2800c2da168519c09da722caba8b
wuaiyi <wuaiyi@youmi.net>
2015-01-12 11:57:30 +0800

    测试版

--- assets/global/global.js
+++ assets/global/global.js
@@ -1,14 +1,33 @@
-var  conHei = $('.wrapper').height() - $('.navbar').height() - $('.bottom').height()
-    ,conOriHei = $('.content').height()
-    ;
-    conHei = conHei > conOriHei ? conHei + 'px' : conOriHei + 'px';
-
-    $('.content').css('height', conHei);
-
-    $(window).resize(function() {
+;(function(win, doc, loc) {
+    /**
+     * 修改 Content 容器的高度
+     * 
+     **/
+    function getConHei() {
         var  conHei = $('.wrapper').height() - $('.navbar').height() - $('.bottom').height()
-        ,conOriHei = $('.content').height()
-        ;
+            ,conOriHei = $('.content').height()
+            ;
         conHei = conHei > conOriHei ? conHei + 'px' : conOriHei + 'px';
         $('.content').css('height', conHei);
-    });
+    }
+
+    (function() {
+        getConHei();
+        
+        // 一系列事件
+        $(win).resize(getConHei);
+
+        $('.main-menu>li').click(function() {
+            var  submenu = $(this).data()
+                ,rec = false;
+                ;
+            submenu = $(submenu.sub);
+            if(submenu.css('display') == 'none') rec = true;
+            $('.cus-submenu').hide();
+            if(rec) { 
+                $(submenu).show(); 
+            }
+        });
+    })();
+})(window, document, location);
+
